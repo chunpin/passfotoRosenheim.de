0243707f2190474ec0d3b37b5290fffca1cb0da6
chunpin <bastianstapelfeldt@Bastians-4Mac-2.fritz.box>
2016-05-18 17:35:38 +0200

    update html

---
+++ scripts/include.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+   
+   $footer = $('#footer');
+   $nav = $('#nav');
+
+
+
+   $nav.load('/include/nav.html');
+   $footer.load('/include/footer.html');
+
+
+
+
+
+
+});
