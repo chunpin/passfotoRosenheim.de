bef9d71a9ee6f91fedd8d6b7db023da4ccb15375
chunpin <bastianstapelfeldt@Bastians-4Mac-2.fritz.box>
2016-06-10 14:24:18 +0200

    add angular into it to make the testimonial section

--- scripts/app.js
+++ scripts/app.js
@@ -3,52 +3,65 @@ var app = angular.module('app', []);
 
 // Define the `testimonialCtrl` controller on the `app` module
 app.controller('testimonialCtrl', function app($scope) {
+  $scope.currentDate = Date.now();
+  $scope.convertToWeekNum = 604800000;
+
+  $scope.headline = {
+    mainText:'Warum unsere Kunden unseren Service lieben',
+    subText:'Weil wir nur mit dem besten Service der Stadt zufrieden sind! Nur wenn du zufrieden bist, sind wir es auch!'
+  }
+
   $scope.comments = [
     { author: 'James',
       text: 'Fast just got faster with Nexus S.',
       img:'/images/passfoto-rosenheim-biometrische-passbilder-personalausweis-visum-fotos-reisepass-quick-shot-kunden-01.png',
       alt:'quick-shot-kunden-01',
-      date: new Date(2016, 3, 02)
+      weeks: Math.round(  ($scope.currentDate -  (new Date("2016-05-01"))) /  $scope.convertToWeekNum  )
     }, 
     { author: 'Veroll',
       text: 'The Next, Next Generation tablet.',
       img:'/images/passfoto-rosenheim-biometrische-passbilder-personalausweis-visum-fotos-reisepass-quick-shot-kunden-02.png',
       alt:'quick-shot-kunden-02',
-      date: new Date(2016, 3, 25)
+      weeks: Math.round(  ($scope.currentDate -  (new Date("2016-3-25"))) /  $scope.convertToWeekNum  )
     }, 
     { author: 'Maya',
       text: 'The Next, Next Generation tablet.',
       img:'/images/passfoto-rosenheim-biometrische-passbilder-personalausweis-visum-fotos-reisepass-quick-shot-kunden-03.png',
       alt:'quick-shot-kunden-03',
-      date: new Date(2016, 2, 28)
-    }
-    , 
+      weeks: Math.round(  ($scope.currentDate -  (new Date("2016-2-28"))) /  $scope.convertToWeekNum  )
+    }, 
     { author: 'Basti',
       text: 'The Next, Next Generation tablet.',
       img:'/images/passfoto-rosenheim-biometrische-passbilder-personalausweis-visum-fotos-reisepass-quick-shot-kunden-04.png',
-      alt:'quick-shot-kunden-03',
-      date: new Date(2016, 1, 28)
-    }
-    , 
+      alt:'quick-shot-kunden-04',
+      weeks: Math.round(  ($scope.currentDate -  (new Date("2016-1-28"))) /  $scope.convertToWeekNum  )
+    }, 
     { author: 'Nani',
       text: 'The Next, Next Generation tablet.',
       img:'/images/passfoto-rosenheim-biometrische-passbilder-personalausweis-visum-fotos-reisepass-quick-shot-kunden-05.png',
-      alt:'quick-shot-kunden-03',
-      date: new Date(2016, 7, 28)
-    }
-    , 
+      alt:'quick-shot-kunden-05',
+      weeks: Math.round(  ($scope.currentDate -  (new Date("2016-5-28"))) /  $scope.convertToWeekNum  )
+    }, 
     { author: 'Paulina',
       text: 'The Next, Next Generation tablet.',
       img:'/images/passfoto-rosenheim-biometrische-passbilder-personalausweis-visum-fotos-reisepass-quick-shot-kunden-06.png',
-      alt:'quick-shot-kunden-03',
-      date: new Date(2016, 6, 09)
-    }
-    , 
+      alt:'quick-shot-kunden-06',
+      weeks: Math.round(  ($scope.currentDate -  (new Date("2016-6-08"))) /  $scope.convertToWeekNum  )
+    }, 
     { author: 'Bibi',
       text: 'The Next, Next Generation tablet.',
       img:'/images/passfoto-rosenheim-biometrische-passbilder-personalausweis-visum-fotos-reisepass-quick-shot-kunden-07.png',
-      alt:'quick-shot-kunden-03',
-      date: new Date(2016, 11, 20 )
+      alt:'quick-shot-kunden-07',
+      weeks: Math.round(  ($scope.currentDate -  (new Date("2016-3-20"))) /  $scope.convertToWeekNum  )
     }
+
   ];
 });
+
+
+
+
+
+
+
+
